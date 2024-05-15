0e0242010186a5b0863cf11f5d21ef8d3c135e4d
dangillman <dangillman93@gmail.com>
2024-05-15 12:53:01 -0400

    added content to index.js, passed tests.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  const data = {
+    name: name,
+    email: email
+  };
+
+  return fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Append the id to the DOM here
+      document.body.innerHTML += data.id;
+    })
+    .catch(error => {
+      // Append the error message to the DOM here
+      document.body.innerHTML += error.message;
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
